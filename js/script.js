7257b2a1cdd490775b54dabcaaaa86e8210c2cac
musicierer <musicierer@gmail.com>
2017-03-23 14:38:46 +0200

    Add new commit
    New commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,59 @@
-var question = {
+var question = [
+	{
 		questionFirst: 'Какая температуа кипения воды?',
 		variantFirst: '60 градусов',
 		variantSecond: '80 градусов',
 		variantThird: '100 градусов'
-	}; 
+	},
+	{
+		questionTwo: 'Сколько метров в километре?',
+		variantFirst: '100 метров',
+		variantSecond: '500 метров',
+		variantThird: '1000 метров'
+	},
+	{
+		questionThree: 'Каким образом шаблон помещается в HTML документ?',
+		variantFirst: 'Шаблон размещают в любом теге',
+		variantSecond: 'Шаблон размещают в теге <script> с нестандартным атрибутом type',
+		variantThird: 'Шаблон размещают в HTML документе как есть, не оборачивая тегом'
+	}
+]; 
 
 localStorage.setItem('test', JSON.stringify(question));
 
 var get = localStorage.getItem('test');
 
+var answer ={
+
+};
+
 $(function() {
 
 	var html = $('#test').html();
-	var question = {
+	var question = [
+	{
 		questionFirst: 'Какая температуа кипения воды?',
 		variantFirst: '60 градусов',
 		variantSecond: '80 градусов',
 		variantThird: '100 градусов'
-	};
+	},
+	{
+		questionFirst: 'Сколько метров в километре?',
+		variantFirst: '100 метров',
+		variantSecond: '500 метров',
+		variantThird: '1000 метров'
+	},
+	{
+		questionFirst: 'В каком объекте хранятся все jQuery-функции?',
+		variantFirst: 'jQuery.fn',
+		variantSecond: ' jQuery.prototype',
+		variantThird: 'jQuery.function'
+	}
+]; 
 
-	var content = tmpl(html, question);
+	var content = tmpl(html, {
+		data: question
+	});
 
 	$('body').append(content);
 
@@ -31,9 +65,6 @@ var $but = $('button');
 
 $but.on('click', function() {
 
-var variantThird = ('#variantThird');
-var variantSecond = ('#variantSecond');
-var variantFirst = ('#variantFirst');
 
 if($('#variantThird').prop('checked')) 
   alert('Правильный ответ');
